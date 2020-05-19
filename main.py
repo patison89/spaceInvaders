1933cfa436ac4c796305aa1f493692c082f44084
patrycja.czerwinska <patison89@gmail.com>
2020-05-19 10:19:35 +0100

    move player on keystrokes

--- main.py
+++ main.py
@@ -15,10 +15,11 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
 
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 
 # Game loop
@@ -30,6 +31,23 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.7
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.7
 
-    player()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    player(playerX, playerY)
+
     pygame.display.update()
