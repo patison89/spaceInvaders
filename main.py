dc2e29a675ae786597f15c3b8fadfdd0288883a7
patrycja.czerwinska <patison89@gmail.com>
2020-05-19 10:16:13 +0100

    add player image

--- main.py
+++ main.py
@@ -11,12 +11,25 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+
 # Game loop
 running = True
 while running:
+
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((0, 0, 0))
+    player()
     pygame.display.update()
