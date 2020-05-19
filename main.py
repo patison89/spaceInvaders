5e01336ba016ea2bc2a8b2f01b95a0825e73def6
patrycja.czerwinska <patison89@gmail.com>
2020-05-19 10:30:37 +0100

    add bullet and bullet movement

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
+# Background
+background = pygame.image.load('space.jpg')
+
 # Player
 playerImg = pygame.image.load('player.png')
 playerX = 370
@@ -24,6 +27,15 @@ enemyX = random.randint(0, 800)
 enemyY = random.randint(0, 150)
 enemyX_change = 0
 
+# Bullet
+
+bulletImg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 10
+bullet_state = "ready"
+
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
@@ -31,6 +43,12 @@ def player(x, y):
 
 def enemy(x, y):
     screen.blit(enemyImg, (x, y))
+
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
 
 
 # Game loop
@@ -47,6 +65,10 @@ while running:
                 playerX_change = -0.7
             if event.key == pygame.K_RIGHT:
                 playerX_change = 0.7
+            if event.key == pygame.K_SPACE:
+                if bullet_state == "ready":
+                    bulletX = playerX
+                    fire_bullet(bulletX, bulletY)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -58,6 +80,15 @@ while running:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
+
+    # Bullet Movement
+    if bulletY <= 0:
+        bulletY = 480
+        bullet_state = "ready"
+
+    if bullet_state == "fire":
+        fire_bullet(bulletX, bulletY)
+        bulletY -= bulletY_change
 
     player(playerX, playerY)
     enemy(enemyX, enemyY)
